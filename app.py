e6318eb05144bba1f126ba7f044576991a19abe3
hukuto7750 <hukuto7750@gmail.com>
2025-06-09 09:19:13 +0800

    update imags

--- app.py
+++ app.py
@@ -1130,7 +1130,7 @@ def handle_message(event):
         "contents": [
           {
             "type": "image",
-            "url": "https://raw.githubusercontent.com/hukluto7750/echo_bot_test01/refs/heads/main/imgs/jimeng_bookroom/png/jimeng_bookroom%20(34).png",
+            "url": "https://raw.githubusercontent.com/hukluto7750/echo_bot_test01/refs/heads/main/imgs/jimeng_bookroom/jpg/jimeng_bookroom%20(34).jpg",
             "size": "full",
             "aspectMode": "cover",
             "aspectRatio": "2:3",
